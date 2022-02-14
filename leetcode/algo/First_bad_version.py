08e13fe36cac6e78816a8f9a304efb69e42cb8fa
Paritosh Tripathi <76477608+paritoshtripathi935@users.noreply.github.com>
2022-02-14 18:47:48 +0530

    Create First_bad_version.py

---
+++ leetcode/algo/First_bad_version.py
@@ -0,0 +1,11 @@
+class Solution:
+    def firstBadVersion(self, n):
+        left = 1 
+        right =  n
+        while(left < right):
+            mid = left + (right-left) // 2
+            if(isBadVersion(mid)):
+                right = mid;
+            else:
+                left = mid + 1
+        return left
