186f800a66aceee9a6899e294f415aaa88c10728
Paritosh Tripathi <76477608+paritoshtripathi935@users.noreply.github.com>
2022-06-07 18:20:36 +0530

    Create Linked_List_Cycle_II.cpp

---
+++ leetcode/linkedlst/Linked_List_Cycle_II.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        ListNode* slow = head;
+      ListNode* fast = head;
+      while(slow && fast && fast->next){
+         slow = slow->next;
+         fast = fast->next->next;
+         if(slow == fast)break;
+      }
+      if(!fast || !fast->next)return NULL;
+      if(slow == fast){
+         slow = head;
+         while(slow!=fast){
+            slow = slow->next;
+            fast = fast->next;
+         }
+      }
+      return slow;
+    }
+};
