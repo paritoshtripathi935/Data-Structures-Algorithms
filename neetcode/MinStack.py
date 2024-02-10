3f22dce699a232b74d8973a76be9e72df7de48fe
PamriPose <tripathiparitosh935@gmail.com>
2024-02-10 22:59:16 +0530

    Create MinStack.py

---
+++ neetcode/MinStack.py
@@ -0,0 +1,44 @@
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+        self.minimum_stack = []
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+
+        if not self.minimum_stack or val <= self.minimum_stack[-1]:
+            self.minimum_stack.append(val)
+
+    def isEmpty(self):
+        return len(self.stack) == 0
+
+    def pop(self) -> None:
+        if self.isEmpty():
+            return
+
+        if self.stack[-1] == self.minimum_stack[-1]:
+            self.minimum_stack.pop()
+
+        self.stack.pop()
+
+    def top(self) -> int:
+        if self.isEmpty():
+            return -1 
+
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        if not self.minimum_stack:
+            return -1 
+
+        return self.minimum_stack[-1]
+
+
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(val)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
